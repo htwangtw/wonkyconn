5f02eac0ac27a3e6bf75d806f5ec45d4bb91a47a
claraElk <clara.elkhantour@gmail.com>
2025-10-22 16:45:35 -0400

    Update test

--- wonkyconn/tests/test_gradients_correlation.py
+++ wonkyconn/tests/test_gradients_correlation.py
@@ -1,10 +1,11 @@
 import nibabel as nib
 import numpy as np
-
+from nilearn.maskers import NiftiLabelsMasker  # type: ignore[import-not-found]
+import glob
 from wonkyconn.features import calculate_gradients_correlation
 
 
-def create_fake_connectivity(n_regions=434):
+def create_fake_connectivity(n_regions=434, n_subjects=1):
     # random symmetric connectivity matrix with diagonal=1
     matrix = np.random.uniform(-1, 1, size=(n_regions, n_regions))
     conn_matrix = (matrix + matrix.T) / 2
@@ -13,23 +14,23 @@ def create_fake_connectivity(n_regions=434):
     return conn_matrix
 
 
-def test_extract_gradients():
-    conn_matrix = create_fake_connectivity()
+def test_gradients():
+    # First test is basically loading the template gradients and just do the correlation to see if correlation = ~0.99
 
     atlas = nib.load("wonkyconn/data/test_data/atlas/atlas-Schaefer2018Combined_dseg.nii.gz")
 
-    ind_aligned_gradient, group_gradients = calculate_gradients_correlation.extract_gradients(conn_matrix, atlas=atlas)
+    conn_matrix = create_fake_connectivity(n_regions=434, n_subjects=3)
 
-    assert group_gradients is not None
-    assert ind_aligned_gradient is not None
+    # Compute gradients from random matrix, extract group gradients
+    random_gradient, group_gradients = calculate_gradients_correlation.extract_gradients(conn_matrix, atlas=atlas)
 
+    # Calculate similarity for random gradient
+    random_individual_spearman = calculate_gradients_correlation.calculate_gradients_similarity(random_gradient, group_gradients)
+    # Calculate average similarity for group gradients
+    random_similarity = calculate_gradients_correlation.calculate_group_gradients_similarity(random_individual_spearman)
 
-def test_calculate_gradients_similarity():
-    conn_matrix = create_fake_connectivity()
-    atlas = nib.load("wonkyconn/data/test_data/atlas/atlas-Schaefer2018Combined_dseg.nii.gz")
+    # Calculate similarity for template gradients (should be high value)
+    template_gradient_similarity = calculate_gradients_correlation.calculate_gradients_similarity(group_gradients, group_gradients)
 
-    ind_aligned_gradient, group_gradients = calculate_gradients_correlation.extract_gradients(conn_matrix, atlas=atlas)
-
-    correlation = calculate_gradients_correlation.calculate_gradients_similarity(ind_aligned_gradient, group_gradients)
-
-    assert correlation is not None
+    assert template_gradient_similarity > 0.99
+    assert random_similarity < template_gradient_similarity
